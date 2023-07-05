ea5c4e15673728958db8103b7951d8d23bdc80dc
lakshaymukhijaLM <90673341+lakshaymukhijaLM@users.noreply.github.com>
2023-07-05 14:03:34 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,17 @@
 import { SessionProvider } from "next-auth/react"
-import Navbar  from "./components/Navbar";
+import Navbar  from "@/components/Navbar";
 import '../styles/globals.css';
-import './components/Navbar.css';
+import '../components/Navbar.css';
+import { Component } from "react";
 
-
-
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
+function MyApp({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
-      <Navbar/>
+    <SessionProvider session={pageProps.session}>
+      <Navbar />
       <Component {...pageProps} />
     </SessionProvider>
   )
 }
 
+export default MyApp
 
