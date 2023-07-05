500949b69e72e6d952855123bf33110b4335a533
Lakshay Mukhija <lakshay.mukhija@nagarro.com>
2023-07-05 13:56:53 +0530

    next-auth

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import { SessionProvider } from "next-auth/react"
+import Navbar  from "./components/Navbar";
+import '../styles/globals.css';
+import './components/Navbar.css';
+
+
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Navbar/>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
+
+
